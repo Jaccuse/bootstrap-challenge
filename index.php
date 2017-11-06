ffe195fde01c082000b2cc1f9b7c81a1a3a6e976
Jaccuse <guck@jack-arnold.com>
2017-11-06 08:27:02 -0700

    Committed the stuff

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- code based on the base project on the bootstrap intro page. I'm pretty sure that's allowed? -->
+        <title>Hello, world!</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="jumbotron">
+            <div class="col-xs-12 col-sm-6 col-md-8">
+                suh
+            </div>
+        </div>
+
+        <!-- borrowed this from Korigan just to see if something would work -->
+        <div class="col-xs-12 col-sm-6 col-md-8">
+            <div class="container">
+                <nav class="navbar navbar-expand-md navbar-light bg-light border border-dark rounded mt-4">
+                    <div class="collapse navbar-collapse" id="navbarText">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Title<span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link1</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link2</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link3</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link4</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </body>
+</html>
